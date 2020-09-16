a9d44c4c7dc63fe24c3a2da5472395ca492487e4
Sinton <yansuntong@gmail.com>
2020-09-17 01:32:36 +0800

    update swarm services's placement constraints  in details

--- src/views/swarm/services/details/placementConstraints.vue
+++ src/views/swarm/services/details/placementConstraints.vue
@@ -2,24 +2,43 @@
   <a-card :bordered="false" style="margin-bottom: 20px;" class="compact">
     <template slot="title"><a-icon type="bars"/> 约束条件</template>
     <a-table :columns="columns"
-             :dataSource="data"
-             :pagination="false">
+             :dataSource="constraints"
+             :pagination="false"
+             :scroll="{y: 200}">
       <template slot="action" slot-scope="text, record">
-        <a-button icon="minus-circle" type="danger" size="small">移除</a-button>
+        <div class="editable-row-operations">
+          <span v-if="record.editable">
+            <a @click="() => save(record)">保存</a>
+            <a-divider type="vertical"/>
+            <a @click="() => record.editable = false">取消</a>
+          </span>
+          <span v-else>
+            <a @click="() => record.editable = true">修改</a>
+            <a-divider type="vertical"/>
+            <a @click="() => remove(record)">移除</a>
+          </span>
+        </div>
       </template>
       <div slot="footer" style="text-align: right;">
         <a style="float:left;">
           <a-button-group>
-            <a-button type="primary" icon="check" size="small" :disabled="!changed">应用变更</a-button>
+            <a-button type="primary" icon="check" size="small" :disabled="!changed" @click="apply">应用变更</a-button>
             <a-dropdown :disabled="!changed">
               <a-button type="default" icon="down" size="small"/>
               <a-menu slot="overlay">
-                <a-menu-item>重置变更</a-menu-item>
+                <a-menu-item @click="reset">重置变更</a-menu-item>
               </a-menu>
             </a-dropdown>
           </a-button-group>
         </a>
-        <a><a-button shape="circle" type="primary" icon="plus" size="small" style="margin-right: 5px"></a-button>添加约束条件</a>
+        <a><a-button shape="circle"
+                     type="primary"
+                     icon="plus"
+                     size="small"
+                     style="margin-right: 5px;"
+                     @click="append"/>
+          添加约束条件
+        </a>
       </div>
     </a-table>
   </a-card>
@@ -44,7 +63,7 @@
             scopedSlots: { customRender: 'Name' }
           },
           {
-            title: '操作',
+            title: '运算符',
             dataIndex: 'Operator',
             sorter: true,
             scopedSlots: { customRender: 'Operator' }
@@ -62,7 +81,58 @@
             scopedSlots: { customRender: 'action' }
           }
         ],
+        constraints: [],
+        originalConstraints: [],
         changed: false
+      }
+    },
+    methods: {
+      renderData() {
+        this.data.forEach(item => {
+          const record = {
+            Name: item.split(' ')[0],
+            Operator: item.split(' ')[1],
+            Value: item.split(' ')[2]
+          }
+          record['editor'] = _.cloneDeep(record)
+          record['editable'] = false
+          this.constraints.push(record)
+        })
+        this.originalConstraints = _.cloneDeep(this.constraints)
+      },
+      append() {
+        const record = {}
+        record['editor'] = {}
+        record['editable'] = true
+        this.constraints.push(record)
+        this.changed = true
+      },
+      save(record) {
+        record = Object.assign(record, record['editor'])
+        record['editable'] = false
+        this.changed = true
+      },
+      remove(record) {
+        const index = this.constraints.indexOf(record)
+        this.constraints.splice(index, 1)
+        this.changed = true
+      },
+      apply() {
+      },
+      reset() {
+        this.constraints = _.cloneDeep(this.originalConstraints)
+        this.changed = false
+      }
+    },
+    watch: {
+      data: {
+        deep: true,
+        immediate: true,
+        handler() {
+          if (!_.isEmpty(this.data)) {
+            this.renderData()
+          }
+        }
       }
     }
   }
