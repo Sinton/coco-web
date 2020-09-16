fd7a4a1604af7a55c91c698ad232d3d5958b02c3
Sinton <yansuntong@gmail.com>
2020-09-17 01:28:53 +0800

    update swarm services's envs in details

--- src/views/swarm/services/details/envs.vue
+++ src/views/swarm/services/details/envs.vue
@@ -4,8 +4,27 @@
     <a-table :columns="columns"
              :dataSource="data"
              :pagination="false">
+      <template v-for="column in columns" v-if="column.scopedSlots" :slot="column.dataIndex" slot-scope="text, record">
+        <div :key="column.dataIndex">
+          <a-input v-if="record.editable"
+                   v-model="record['editor'][column.dataIndex]"
+                   style="margin: -6px 0"/>
+          <template v-else>{{ text }}</template>
+        </div>
+      </template>
       <template slot="action" slot-scope="text, record">
-        <a-button type="danger" icon="minus-circle" size="small">移除</a-button>
+        <div class="editable-row-operations">
+          <span v-if="record.editable">
+            <a @click="() => save(record)">保存</a>
+            <a-divider type="vertical"/>
+            <a @click="() => record.editable = false">取消</a>
+          </span>
+          <span v-else>
+            <a @click="() => record.editable = true">修改</a>
+            <a-divider type="vertical"/>
+            <a @click="() => remove(record)">移除</a>
+          </span>
+        </div>
       </template>
       <div slot="footer" style="text-align: right;">
         <a style="float:left;">
@@ -38,28 +57,41 @@
       return {
         columns: [
           {
-            title: '标签',
-            dataIndex: 'Label',
-            sorter: true,
-            scopedSlots: { customRender: 'Label' }
+            title: '变量名',
+            dataIndex: 'name',
+            scopedSlots: { customRender: 'name' }
           },
           {
-            title: '值',
-            dataIndex: 'Value',
-            sorter: true,
-            scopedSlots: { customRender: 'Value' }
+            title: '变量值',
+            dataIndex: 'value',
+            scopedSlots: { customRender: 'value' }
           },
           {
             title: '操作',
             dataIndex: 'action',
-            width: 150,
-            scopedSlots: { customRender: 'action' }
+            scopedSlots: { customRender: 'action' },
+            width: 120
           }
         ],
         changed: false
       }
     },
-    methods: {}
+    methods: {
+      append() {
+        const record = {}
+        record['editor'] = _.cloneDeep(record)
+        record.editable = true
+        this.data.push(record)
+      },
+      save(record) {
+        record = Object.assign(record, record['editor'])
+        record.editable = false
+      },
+      remove(record) {
+        const index = this.data.indexOf(record)
+        this.data.splice(index, 1)
+      }
+    }
   }
 </script>
 
