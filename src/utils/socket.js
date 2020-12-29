9d13bd1965b0a208343a2cbd7d24f6b2695eb38a
Sinton <yansuntong@gmail.com>
2020-12-30 00:03:49 +0800

    refactor socket.io auth logic

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
-import Cookies from 'js-cookie'
 import { ACCESS_TOKEN } from '@/store/mutation-types'
 import VueSocketIO from 'vue-socket.io'
 
 export function socketConnect() {
-  if(Cookies.get(ACCESS_TOKEN)) {
+  if(Vue.ls.get(ACCESS_TOKEN)) {
+    const authenticate = {}
+    authenticate[ACCESS_TOKEN] = Vue.ls.get(ACCESS_TOKEN)
     Vue.use(new VueSocketIO({
-      debug: true,
-      connection: 'http://localhost:9099'
+      debug: !(process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true'),
+      connection: 'http://localhost:9099',
+      options: {
+        query: authenticate
+      }
     }))
   }
 }
