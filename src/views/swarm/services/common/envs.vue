bc5b3238f6a224756d4410c2aa42704264f75a38
Sinton <yansuntong@gmail.com>
2021-09-21 16:22:39 +0800

    refactor swarm service support Service's Envs update and details display feature

---
+++ src/views/swarm/services/common/envs.vue
@@ -0,0 +1,47 @@
+<template>
+  <coco-editor-table :columns="columns" :data="data" :button-text="'添加环境变量'" @save="() => this.$emit('changed')">
+    <slot name="applyOperation" slot="footerLeft"></slot>
+  </coco-editor-table>
+</template>
+
+<script>
+  import { CocoEditorTable } from '@/components/Coco'
+
+  export default {
+    name: 'CommonServiceEnvs',
+    components: {
+      CocoEditorTable
+    },
+    props: {
+      data: {
+        type: Array,
+        default: () => []
+      }
+    },
+    data() {
+      return {
+        columns: [
+          {
+            title: '变量名',
+            dataIndex: 'name',
+            scopedSlots: { customRender: 'name' }
+          },
+          {
+            title: '变量值',
+            dataIndex: 'value',
+            scopedSlots: { customRender: 'value' }
+          },
+          {
+            title: '操作',
+            dataIndex: 'action',
+            scopedSlots: { customRender: 'action' },
+            width: 120
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+</style>
--- src/views/swarm/services/details/envs.vue
+++ src/views/swarm/services/details/envs.vue
@@ -1,61 +1,35 @@
 <template>
   <a-card :bordered="false" style="margin-bottom: 20px;" class="compact">
     <template slot="title"><a-icon type="bars"/> 环境变量</template>
-    <a-table :columns="columns"
-             :dataSource="envs"
-             :pagination="false">
-      <template v-for="column in columns" v-if="column.scopedSlots" :slot="column.dataIndex" slot-scope="text, record">
-        <div :key="column.dataIndex">
-          <a-input v-if="record.editable"
-                   v-model="record['editor'][column.dataIndex]"
-                   :disabled="column.dataIndex === 'name' && record['original']"
-                   style="margin: -6px 0"/>
-          <template v-else>{{ text }}</template>
-        </div>
-      </template>
-      <template slot="action" slot-scope="text, record">
-        <div class="editable-row-operations">
-          <span v-if="record.editable">
-            <a @click="() => save(record)">保存</a>
-            <a-divider type="vertical"/>
-            <a @click="() => record.editable = false">取消</a>
-          </span>
-          <span v-else>
-            <a @click="() => record.editable = true">修改</a>
-            <a-divider type="vertical"/>
-            <a @click="() => remove(record)">移除</a>
-          </span>
-        </div>
-      </template>
-      <div slot="footer" style="text-align: right;">
-        <a style="float:left;">
-          <a-button-group>
-            <a-button type="primary" icon="check" size="small" :disabled="!changed" @click="apply">应用变更</a-button>
-            <a-dropdown :disabled="!changed">
-              <a-button type="default" icon="down" size="small"/>
-              <a-menu slot="overlay">
-                <a-menu-item @click="reset">重置变更</a-menu-item>
-              </a-menu>
-            </a-dropdown>
-          </a-button-group>
-        </a>
-        <a><a-button shape="circle"
-                     type="primary"
-                     icon="plus"
-                     size="small"
-                     style="margin-right: 5px;"
-                     @click="append"/>
-          添加环境变量
-        </a>
-      </div>
-    </a-table>
+    <common-service-envs :data="envs" @changed="() => this.changed = true">
+      <a-button-group slot="applyOperation">
+        <a-button type="primary" icon="check" size="small" :disabled="!changed" @click="onApply">应用变更</a-button>
+        <a-dropdown :disabled="!changed">
+          <a-button type="default" icon="down" size="small"/>
+          <a-menu slot="overlay">
+            <a-menu-item @click="onReset">重置变更</a-menu-item>
+          </a-menu>
+        </a-dropdown>
+      </a-button-group>
+    </common-service-envs>
   </a-card>
 </template>
 
 <script>
+  import CommonServiceEnvs from '@/views/swarm/services/common/envs'
+  import { cloneDeep, isNotEmpty } from '@/utils/util'
+  import { invokeApi } from '@/api/http'
+
   export default {
-    name: 'ServiceEnvs',
+    name: 'DetailsServiceServiceEnvs',
+    components: {
+      CommonServiceEnvs
+    },
     props: {
+      id: {
+        type: String,
+        default: () => ''
+      },
       data: {
         type: Array,
         default: () => []
@@ -63,24 +37,6 @@
     },
     data() {
       return {
-        columns: [
-          {
-            title: '变量名',
-            dataIndex: 'name',
-            scopedSlots: { customRender: 'name' }
-          },
-          {
-            title: '变量值',
-            dataIndex: 'value',
-            scopedSlots: { customRender: 'value' }
-          },
-          {
-            title: '操作',
-            dataIndex: 'action',
-            scopedSlots: { customRender: 'action' },
-            width: 120
-          }
-        ],
         envs: [],
         originalEnvs: [],
         changed: false
@@ -90,36 +46,31 @@
       renderData() {
         this.data.forEach(item => {
           const record = { name: item.split('=')[0], value: item.split('=')[1] }
-          record['editor'] = _.cloneDeep(record)
+          record['editor'] = cloneDeep(record)
           record['editable'] = false
           record['original'] = true
           this.envs.push(record)
         })
-        this.originalEnvs = _.cloneDeep(this.envs)
+        this.originalEnvs = cloneDeep(this.envs)
       },
-      append() {
-        const record = {}
-        record['editor'] = _.cloneDeep(record)
-        record['editable'] = true
-        record['original'] = false
-        this.envs.push(record)
-        this.changed = true
+      onApply() {
+        const params = { serviceId: this.id }
+        params['envs'] = this.envs
+        invokeApi('/service/update', params).then(response => {
+          if (response.code === 2000) {
+            this.changed = false
+            this.$notification.success({ message: '标题', description: '更新服务配置成功' })
+            this.$router.go(0)
+          } else {
+            this.$notification.warning({ message: '标题', description: '更新服务配置失败' })
+          }
+        }).catch(() => {
+          this.$notification.error({ message: '错误', description: '更新服务配置异常' })
+        })
       },
-      save(record) {
-        record = Object.assign(record, record['editor'])
-        record['editable'] = false
-        this.changed = true
-      },
-      remove(record) {
-        const index = this.data.indexOf(record)
-        this.envs.splice(index, 1)
-        this.changed = true
-      },
-      apply() {
-      },
-      reset() {
-        this.envs = _.cloneDeep(this.originalEnvs)
+      onReset() {
         this.changed = false
+        this.envs = cloneDeep(this.originalEnvs)
       }
     },
     watch: {
@@ -127,7 +78,7 @@
         deep: true,
         immediate: true,
         handler() {
-          if (!_.isEmpty(this.data)) {
+          if (isNotEmpty(this.data)) {
             this.renderData()
           }
         }
